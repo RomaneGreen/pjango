8b7b98eb9149e1b0b843ea2e568368334a5c4dae
Romane G <romane71193@gmail.com>
2019-01-22 22:45:08 -0500

    user profile in django admin

--- renmoproj/renmoproj/urls.py
+++ renmoproj/renmoproj/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('auth/', include('social_django.urls', namespace='social')),
+    
+
    
 ]
