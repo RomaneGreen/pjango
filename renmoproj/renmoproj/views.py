cf9be6dc0c9000fc4dbf5b5bb79e2643ad8c0487
Romane G <romane71193@gmail.com>
2019-01-25 18:20:53 -0500

    fix urls

--- renmoproj/renmoproj/views.py
+++ renmoproj/renmoproj/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from renmo.forms import RegistrationForm
 
 
 def home(request):
@@ -10,11 +12,11 @@ def home(request):
 
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('/home')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
         args = {'form': form}
         return render(request, 'registration/reg_form.html', args)
